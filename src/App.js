cc1e49b26237d88e49262b2b10fea77f17c77a66
Anjaraniaina <hei.anjaraniaina@gmail.com>
2023-02-07 15:00:15 +0300

    chore: add tests

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import DoneList from './components/DoneList';
+import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
+
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [doneTodos, setDoneTodos] = useState([]);
+
+  const addTodo = (todo) => {
+    setTodos([...todos, todo]);
+  };
+
+  const completeTodo = (index) => {
+    setDoneTodos([...doneTodos, todos[index]]);
+    setTodos(todos.filter((_, i) => i !== index));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className='todo'>
+        <h1>Todo</h1>
+        <TodoForm addTodo={addTodo} />
+        <TodoList todos={todos} completeTodo={completeTodo} />
+      </div>
+      <div className='done'>
+        <h1>Done</h1>
+        <DoneList doneTodos={doneTodos} />
+      </div>
     </div>
   );
 }
